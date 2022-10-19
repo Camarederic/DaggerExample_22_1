8d1a96c78e7b2964bdbc164b9628fd8c09d396a3
Camarederic <camarederic@mail.ru>
2022-10-19 23:18:40 +0300

    Custom Builder, BindsInstance

--- app/src/main/java/com/example/daggerexample_22_1/di/ApplicationComponent.kt
+++ app/src/main/java/com/example/daggerexample_22_1/di/ApplicationComponent.kt
@@ -1,10 +1,26 @@
 package com.example.daggerexample_22_1.di
 
+import android.content.Context
 import com.example.daggerexample_22_1.presentation.MainActivity
+import dagger.BindsInstance
 import dagger.Component
 
-@Component(modules = [DataModule::class, DomainModule::class, ContextModule::class])
+@Component(modules = [DataModule::class, DomainModule::class])
 interface ApplicationComponent {
 
     fun inject(activity: MainActivity)
+
+    @Component.Builder
+    interface ApplicationComponentBuilder{
+
+        @BindsInstance
+        fun context(context: Context): ApplicationComponentBuilder
+
+        @BindsInstance
+        fun timeMillis(timeMillis:Long): ApplicationComponentBuilder
+
+        fun build(): ApplicationComponent
+
+
+    }
 }
